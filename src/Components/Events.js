29cfe6d649ccbd6ab580e41e3645a43bc5bfc8e1
Nishant <nishanttomar910@gmail.com>
2024-01-13 16:18:45 +0530

    initial commit

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import Styles from '../Styles/Events.module.css'
