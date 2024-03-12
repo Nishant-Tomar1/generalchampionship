696dbf87a06530b85800702ac238e63dca69756b
AShr12xz <122085103+AShr12xz@users.noreply.github.com>
2024-03-12 12:23:16 +0530

    Refactor Leaderboard component and update data

--- src/Pages/Leaderboard.tsx
+++ src/Pages/Leaderboard.tsx
@@ -1,54 +1,58 @@
-import React from 'react'
-import Leader from '../Components/Leader.tsx'
-import LeaderboardTable from '../Components/LeaderBoard/leaderboard_table.tsx'
-import {motion} from 'framer-motion'
-import { Data } from '../Components/LeaderBoard/model.tsx'
+import React from "react";
+import Leader from "../Components/Leader.tsx";
+import LeaderboardTable from "../Components/LeaderBoard/leaderboard_table.tsx";
+import { motion } from "framer-motion";
+import { Data } from "../Components/LeaderBoard/model.tsx";
 
 export const data: Data[] = [
   {
     branch: "EE",
-    score: "0",
+    score: "300",
   },
   {
     branch: "CSE",
-    score: "0",
+    score: "600",
   },
   {
     branch: "ECE-META",
-    score: "0",
+    score: "100",
   },
-  { branch: "MECH", score: "0" },
+  { branch: "MECH", score: "300" },
   { branch: "MSC-ITEP", score: "0" },
   { branch: "MTECH", score: "0" },
   { branch: "PHD", score: "0" },
-  { branch: "CIVIL", score: "0" },
+  { branch: "CIVIL", score: "1700" },
 ];
 
-
-
-function Leaderboard() {  const variant = {
-  entry : {
-      opacity: [0,1], 
-      transition:{
-          duration:0.25,
-          times:[0,1],
-      }
-  },
-  exit : {
-      opacity: [1,0], 
-      transition:{
-          duration:0.25,
-          times:[0,1],
-      }
-  },
-}
+function Leaderboard() {
+  const variant = {
+    entry: {
+      opacity: [0, 1],
+      transition: {
+        duration: 0.25,
+        times: [0, 1],
+      },
+    },
+    exit: {
+      opacity: [1, 0],
+      transition: {
+        duration: 0.25,
+        times: [0, 1],
+      },
+    },
+  };
 
   return (
-    <motion.div className='flex flex-col items-center justify-center pt-16' variants={variant} animate='entry' exit='exit'>
-      <Leader data={data}/>
-      <LeaderboardTable data={data}/>
+    <motion.div
+      className="flex flex-col items-center justify-center pt-16"
+      variants={variant}
+      animate="entry"
+      exit="exit"
+    >
+      <Leader data={data} />
+      <LeaderboardTable data={data} />
     </motion.div>
-  )
+  );
 }
 
-export default Leaderboard
+export default Leaderboard;
